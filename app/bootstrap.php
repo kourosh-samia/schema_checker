c1474144e573ce57d8170ffc3a55c5a67a6949c2
k.samia.k@gmail.com <k.samia.k@gmail.com>
2016-06-01 08:55:56 -0400

    bootstrap been added

---
+++ app/bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+require_once('Command.php');
+require_once('Config.php');
+require_once('Functions.php');
+require_once('Models.php');
+require_once('Db.php');
+require_once('Rules.php');
+require_once('Exphandler/Exphandler.php');
+require_once('Exphandler/General.php');
+
+require_once('Config/RulesConfig.php');
+
+
+// function autoload($class) {
+// 	$file_name ='../'.$class;
+// //echo $file_name;die;
+// 	require_once ($file_name);
+// }
+
+// spl_autoload_register('autoload');
